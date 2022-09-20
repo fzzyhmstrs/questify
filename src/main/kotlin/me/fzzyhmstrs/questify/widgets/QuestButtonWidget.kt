8283e834b6d14bfbec6366f0bddbdfd8ab0ed6b0
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-09-20 14:45:02 -0400

    Update QuestButtonWidget.kt

--- src/main/kotlin/me/fzzyhmstrs/questify/widgets/QuestButtonWidget.kt
+++ src/main/kotlin/me/fzzyhmstrs/questify/widgets/QuestButtonWidget.kt
@@ -6,12 +6,27 @@ import net.minecraft.client.gui.widget.ToggleButtonWidget
 import net.minecraft.item.Item
 import net.minecraft.item.Items
 
-class QuestButtonWidget(x: Int, y: Int, height: Int, width: Int, toggled: Boolean, questId: Int, icon: Item = Items.WRITABLE_BOOK): ToggleButtonWidget(x, y, width, height, toggled) {
+class QuestButtonWidget(x: Int, y: Int, u: Int, v: Int, height: Int, width: Int, quest: ClientQuest, icon: Item = Items.WRITABLE_BOOK, pressAction: PressAction): 
+CustomToggleButtonWidget(x, y, u, v, width, height, 22, 22, quest.tooltip, openQuestWindowAction) {
 
-    private val arrows: Map<Vertex,AbstractArrow> = mutableMapOf()
-
+    private val openQuestWindowAction = PressAction {alertable -> TextureUtils.openMenuWidget(quest.questMenu)}
+    
     fun hasArrows(): Boolean{
-        return arrows.isNotEmpty()
+        return quest.hasArrows()
+    }
+    
+    fun setQuestAlert(){
+        if (quest.isCompleted && !quest.rewardsTaken){
+            setAlerted(AlertUtils.IconAlert.GOLD_EXCLAIM)
+        } else if (quest.isCompleted && quest.rewardsTaken){
+            setAlerted(AlertUtils.IconAlert.GOLD_CHECK)
+        }
+    }
+    
+    //need to check for a right click method...
+    
+    override fun renderButtonIcon(matrices: MatrixStack, mouseX: Int, mouseY: Int, delta: Float){
+        //renderItem here
     }
 
 }
